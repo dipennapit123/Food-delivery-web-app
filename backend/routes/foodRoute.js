dae1748d0ba4f32298078077f936da1f5e383f12
dipennapit123 <dipennapit45@gmail.com>
2024-08-17 13:24:45 +0545

    creating routes for storing data from from models

---
+++ backend/routes/foodRoute.js
@@ -0,0 +1,11 @@
+import express from "express"
+import { addFood } from "../controllers/foodController.js"
+import multer from "multer" //image storage system
+
+const foodRouter=express.Router();
+
+
+
+
+
+export default foodRouter;
