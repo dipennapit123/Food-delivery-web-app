0b88b3985af64b51ed6b6ee680d498b9101434f8
dipennapit123 <dipennapit45@gmail.com>
2024-08-20 04:59:50 +0545

    implementation of food delivery status on admin

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -19,6 +19,19 @@ const Orders = ({ url }) => {
     }
   };
 
+  const statusHandler=async(event,orderId)=>{
+    const response=await axios.post(url+"/api/order/status",{
+      orderId,
+      status:event.target.value
+    })
+    if (response.data.success){
+      await fetchAllOrders()
+    }
+    
+
+
+  }
+
   useEffect(() => {
     fetchAllOrders();
   }, []);
@@ -48,7 +61,7 @@ const Orders = ({ url }) => {
             </div>
             <p>Items:{order.items.length}</p>
             <p>${order.amount}</p>
-            <select name="" id="">
+            <select onChange={(event)=>statusHandler(event,order._id)} value={order.status}>
               <option value="Food processing">Food processing</option>
               <option value="Out for delivery">Out for delivery</option>
               <option value="Delivered">Delivered</option>
